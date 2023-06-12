fa8227171b88f5ad99dd1aed35e2ef61a074e688
Человек <nik21.dance@gmail.com>
2023-06-12 17:48:41 +0700

    Добавил валидацию по запросу, cохранение и подстановка значений из последнего запроса,и создал класс

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -21,11 +21,11 @@ class CategoryRequest extends FormRequest
      */
     public function rules(): array
     {
-     $rules = [
-         'code' => 'required|min:3|max:255',
-         'name' => 'required|min:3|max:255',
-         'description' => 'required|min:5',
-     ];
+        $rules = [
+            'code' => 'required|min:3|max:255',
+            'name' => 'required|min:3|max:255',
+            'description' => 'required|min:5',
+        ];
         if ($this->route()->named('categories.update')){
             $rules['code'] .= ',' . $this->route()->parameter('category')->id;
         }
